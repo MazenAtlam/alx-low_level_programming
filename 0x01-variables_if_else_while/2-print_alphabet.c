e6ce6cf174a0c57162d1165d7aa56e34882674c2
MazenAtlam <mazenatef5510@gmail.com>
2023-07-14 11:18:02 -0700

    task31

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char alph[] = {abcdefghijklmnopqrstuvwxyz};
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
 	while (i < 26)
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,22 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * Description: A C program that prints the alphabet in lower/uppercase
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        char alph[] = {abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ};
-        int i = 0;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
 
-        while (i < 52)
-        {
-                putchar (alph[i]);
-                i++;
-        }
-        putchar ('\n');
+	while (i < 52)
+	{
+		putchar (alph[i]);
+		i++;
+	}
+	putchar ('\n');
 
-        return (0);
+	return (0);
 }
