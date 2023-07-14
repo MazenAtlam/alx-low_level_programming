76ca373572dcfc6f8e1946662501a8af1ab7b09d
MazenAtlam <mazenatef5510@gmail.com>
2023-07-14 16:52:08 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints all differentCombinationsOf 2-2 digits
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0, j = 0, k = 0, z = 0;
+
+	while (i <= 9)
+	{
+		while (j <= 8)
+		{
+			while (k <= 9)
+			{
+				while (z <= 9)
+				{
+					if (i + j < k + z)
+					{
+						putchar(i + 48);
+						putchar(j + 48);
+						putchar(' ');
+						putchar(k + 48);
+						putchar(z + 48);
+					if (i != 9 || j != 8 || k != 9 || z != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+					z++;
+				}
+				k++;
+				z = 0;
+			}
+			j++;
+			k = 0;
+			z = 0;
+		}
+		i++;
+		j = 0;
+		k = 0;
+		z = 0;
+	}
+	putchar('\n');
+	return (0);
+}
