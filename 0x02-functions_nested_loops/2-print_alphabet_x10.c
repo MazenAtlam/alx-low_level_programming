3abad5c5606ebcdf92b1a0e2437e407fb6db7a5f
MazenAtlam <mazenatef5510@gmail.com>
2023-07-18 12:30:33 -0700

    task2, 5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabett_x10 - A function that prints 10 times alphabet in lowercase
+ * print_alphabet_x10 - A function that prints 10 times alphabet in lowercase
  *			,followed by a new line
  *
  * Return: Null
