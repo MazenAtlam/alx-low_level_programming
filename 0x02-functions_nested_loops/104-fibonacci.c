a58f3f8f80c4a975dc4d83170048fa3430b48c58
MazenAtlam <mazenatef5510@gmail.com>
2023-07-19 16:29:46 -0700

    task16, 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,78 @@
 #include <stdio.h>
 
 /**
+ * numLenght - A function to compute the number lenght
+ * @num: The number that its lenght is computed
+ *
+ * Return: The lenght of the number
+*/
+
+int numLenght(unsigned long num)
+{
+	int lenght = 0;
+
+	if (num == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		while (num != 0)
+		{
+			num /= 10;
+			lenght++;
+		}
+		return (lenght);
+	}
+}
+
+/**
  * main - Entry point
  *
  * Description: A C program that prints the first 98 Fibonacci numbers
- *				, starting with 1 and 2
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int i;
-	unsigned long ldigit, lsum, sum;
+	unsigned long fd = 1, sd = 2, i = 2, mx = 1000000000, o_fd, o_sd, sum, o_sum;
 
-	ldigit = 1;
-	lsum = 2;
-	printf("%lu, %lu, ", ldigit, lsum);
-	for (i = 3; i <= 98; i++)
+	printf("%lu, %lu, ", fd, sd);
+	while (i <= 98)
 	{
-		sum = lsum + ldigit;
-		ldigit = lsum;
-		lsum = sum;
+		sum = fd + sd;
+		if (numLenght(sum) >= numLenght(mx))
+		{
+			break; }
+		fd = sd;
+		sd = sum;
+		printf("%lu, ", sum);
+		i++; }
+	o_fd = fd / 100000;
+	fd %= 100000;
+	o_sd = sd / 100000;
+	sd %= 100000;
+	for (; i <= 98; i++)
+	{
+		sum = fd + sd;
+		o_sum = o_fd + o_sd;
+		fd = sd;
+		o_fd = o_sd;
+		sd = sum;
+		o_sd = o_sum;
 		if (i == 98)
 		{
-			printf("%lu\n", sum);
-		}
+			unsigned long k = sum;
+
+			sum = ((o_sum * 100000) + sum) % 100000;
+			o_sum = ((o_sum * 100000) + k) / 100000;
+			printf("%lu%lu\n", o_sum, sum); }
 		else
 		{
-			printf("%lu, ", sum);
-		}
-	}
-	return (0);
-}
+			unsigned long k = sum;
+
+			sum = ((o_sum * 100000) + sum) % 100000;
+			o_sum = ((o_sum * 100000) + k) / 100000;
+			printf("%lu%lu, ", o_sum, sum);	}	}
+	return (0); }
