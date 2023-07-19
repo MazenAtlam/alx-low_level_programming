32d684b2fcc04ebb77d7e94fc787e5dda2d47686
MazenAtlam <mazenatef5510@gmail.com>
2023-07-18 18:39:59 -0700

    task14, 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,43 +11,28 @@
 
 int main(void)
 {
-	int i;
+	long i;
 	int j = 2;
 
 	int a[50];
 
-	a[0] = 0;
-	a[1] = 1;
+	a[0] = 1;
+	a[1] = 2;
 	printf("%d, %d, ", a[0], a[1]);
-	for (i = 1; j <= 49; i++)
+	for (i = 3; j <= 49; i++)
 	{
-		int num = i;
-
-		while (num >= 10)
-		{
-			num /= 10;
-		}
 		if (i == a[j - 1] + a[j - 2])
 		{
-			int num = i;
-
-			while (num >= 10)
+			a[j] = i;
+			if (j == 49)
 			{
-				num /= 10;
+				printf("%d\n", a[j]);
 			}
-			if ((num == 1) || (num == 2))
+			else
 			{
-				a[j] = i;
-				if (j == 49)
-				{
-					printf("%d\n", a[j]);
-				}
-				else
-				{
-					printf("%d, ", a[j]);
-				}
-				j++;
+				printf("%d, ", a[j]);
 			}
+			j++;
 		}
 	}
 	return (0);
