b9450284d222688730e6b65feff3581d720e3e87
MazenAtlam <mazenatef5510@gmail.com>
2023-07-19 20:01:34 -0700

    task16, 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,61 +1,6 @@
 #include <stdio.h>
 
 /**
- * numLenght - A function to compute the number lenght
- * @num: The number that its lenght is computed
- *
- * Return: The lenght of the number
-*/
-
-int numLenght(unsigned long num)
-{
-	int lenght = 0;
-
-	if (num == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		while (num != 0)
-		{
-			num /= 10;
-			lenght++;
-		}
-	return (lenght);
-	}
-}
-
-/**
- * check_2 - A function that compare Lenght
- * @a: The number to be compared
- * @b: The returned number
- *
- * Return: The edited number
-*/
-
-void check(unsigned long a, unsigned long & b);
-{
-	int d = 8 - numLenght(a);
-
-	if (d != 0)
-	{
-		if (d == 1)
-		{
-			b *= 10;
-		}
-		else if (d == 2)
-		{
-			b *= 100;
-		}
-		else if (d == 3)
-		{
-			b *= 1000;
-		}
-	}
-}
-
-/**
  * main - Entry point
  *
  * Description: A C program that prints the first 98 Fibonacci numbers
@@ -79,8 +24,6 @@ int main(void)
 	fd %= z;
 	o_sd = sd / z;
 	sd %= z;
-	void check(fd, &o_fd);
-	void check(sd, &0_sd);
 	for (i = 51; i <= 98; i++)
 	{
 		sum = fd + sd;
@@ -94,13 +37,11 @@ int main(void)
 			k = sum;
 			sum = ((o_sum * z) + sum) % z;
 			o_sum = ((o_sum * z) + k) / z;
-			void check_2(sum, &o_sum);
 			printf("%lu%lu\n", o_sum, sum); }
 		else
 		{
 			k = sum;
 			sum = ((o_sum * z) + sum) % z;
 			o_sum = ((o_sum * z) + k) / z;
-			void check_2(sum, &o_sum);
 			printf("%lu%lu, ", o_sum, sum); }	}
 	return (0); }
