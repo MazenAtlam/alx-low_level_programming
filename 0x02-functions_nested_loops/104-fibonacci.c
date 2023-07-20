b7aa39a77c077a1579dad409f7e1f2424b8595fa
MazenAtlam <mazenatef5510@gmail.com>
2023-07-19 17:38:00 -0700

    task16, 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	unsigned long fd = 1, sd = 2, i = 2, o_fd, o_sd, sum, o_sum, k;
+	unsigned long fd = 1, sd = 2, i = 2, k, o_fd, o_sd, sum, o_sum;
 
 	printf("%lu, %lu, ", fd, sd);
 	while (i <= 50)
@@ -19,12 +19,13 @@ int main(void)
 		fd = sd;
 		sd = sum;
 		printf("%lu, ", sum);
-		i++; }
+		i++;
+	}
 	o_fd = fd / 100000;
 	fd %= 100000;
 	o_sd = sd / 100000;
 	sd %= 100000;
-	for (i = 51; i <= 91; i++)
+	for (i = 51; i <= 98; i++)
 	{
 		sum = fd + sd;
 		o_sum = o_fd + o_sd;
@@ -32,11 +33,19 @@ int main(void)
 		o_fd = o_sd;
 		sd = sum;
 		o_sd = o_sum;
-		k = sum;
-		sum = ((o_sum * 100000) + sum) % 100000;
-		o_sum = ((o_sum * 100000) + k) / 100000;
-		printf("%lu%lu, ", o_sum, sum); }
-	printf("19740274219868223167, 31940434634990099905, 51680708854858323072, ");
-	printf("83621143489848422977, 135301852344706746049, 218922995834555169026");
-	printf(", 354224848179261915075\n");
-	return (0); }
+		if (i == 98)
+		{
+			k = sum;
+			sum = ((o_sum * 100000) + sum) % 100000;
+			o_sum = ((o_sum * 100000) + k) / 100000;
+			printf("%lu%lu\n", o_sum, sum);
+		}
+		else
+		{
+			k = sum;
+			sum = ((o_sum * 100000) + sum) % 100000;
+			o_sum = ((o_sum * 100000) + k) / 100000;
+			printf("%lu%lu, ", o_sum, sum);
+	}	}
+	return (0);
+}
