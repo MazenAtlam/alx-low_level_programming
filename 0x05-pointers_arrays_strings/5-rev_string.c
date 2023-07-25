d47d521ee3621a40e2caa13de2605a33a42554d8
MazenAtlam <mazenatef5510@gmail.com>
2023-07-24 20:01:39 -0700

    task5, 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -26,6 +26,34 @@ int _strlen(char *s)
 }
 
 /**
+ * _strcpy - A function that copies a string pointed to
+ * @dest: The returned string
+ * @src: The sring to be copied
+ *
+ * Return: The pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		else
+		{
+			dest[i] = '\0';
+			break;
+		}
+	}
+	return (dest);
+}
+
+/**
  * rev_string - A function that reverses a string
  * @s: The string
  *
@@ -35,12 +63,12 @@ int _strlen(char *s)
 void rev_string(char *s)
 {
 	int lenght = _strlen(s), j = lenght - 1, i;
-	char ch;
+	char *ch1 = "", *ch2;
 
+	ch2 = _strcpy(ch1, s);
 	for (i = 0; i < lenght; i++)
 	{
-		ch = s[j];
-		s[i] = ch;
+		s[i] = ch2[j];
 		j--;
 	}
 }
