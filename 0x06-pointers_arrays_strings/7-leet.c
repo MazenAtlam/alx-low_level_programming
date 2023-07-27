8cf105f8912fa7ab2b53895c098c992a10b2c8ea
MazenAtlam <mazenatef5510@gmail.com>
2023-07-26 19:02:04 -0700

    task7, 1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,9 +10,9 @@
 char *leet(char *str)
 {
 	int i = 0;
-	int *pass = {4, 3, 0, 7, 1};
-	char *lower = {'a', 'e', 'o', 't', 'l', '\0'};
-	char *upper = {'A', 'E', 'O', 'T', 'L', '\0'};
+	int pass[] = {4, 3, 0, 7, 1};
+	char lower[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L', '\0'};
 
 	while (str[i] != '\0')
 	{
@@ -20,9 +20,9 @@ char *leet(char *str)
 
 		while (lower[j] != '\0' && upper[j] != '\0')
 		{
-			if (str[j] == lower[j] || str[j] == upper[j])
+			if (str[i] == lower[j] || str[i] == upper[j])
 			{
-				str[j] = pass[j];
+				str[i] = pass[j] + 48;
 			}
 			j++;
 		}
