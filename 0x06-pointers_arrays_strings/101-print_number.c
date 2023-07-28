7cdc141f1f670bf3e0b2adf9f101d04233a949c0
MazenAtlam <mazenatef5510@gmail.com>
2023-07-28 09:46:20 -0700

    task9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * numLength - A function that computes the length of an integer
+ * @num: The integer
+ * Return: The length
+*/
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (num >= -9 && num <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		while (num != 0)
+		{
+			num = num / 10;
+			length++;
+		}
+	}
+
+	return (length);
+}
+
+/**
+ * print_number - A function that prints an integer
+ * @n: The integer
+ *
+ * Return: Nothing
+*/
+
+void print_number(int n)
+{
+	int digit, denom = 1, zeros = numLength(n) - 1;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	while (zeros != 0)
+	{
+		denom = denom * 10;
+		zeros--;
+	}
+	while (denom != 0)
+	{
+		digit = n / denom;
+		n = n % denom;
+		denom = denom / 10;
+		_putchar(digit + '0');
+		if (numLength(n) < numLength(denom))
+		{
+			_putchar('0');
+			denom = denom / 10;
+		}
+	}
+}
