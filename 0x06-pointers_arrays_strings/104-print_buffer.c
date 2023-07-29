08324ca00b2cb9d1f8295015b240586bc141bef8
MazenAtlam <mazenatef5510@gmail.com>
2023-07-29 09:39:35 -0700

    task12, 1

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,37 +14,29 @@ void print_buffer(char *b, int size)
 	int i = 0, j;
 
 	if (size <= 0)
-	{
-		putchar('\n');
-	}
+	{	putchar('\n');	}
 	else
 	{
 		while (i < size)
 		{
 			printf("%08x: ", i);
-			for (j = i; j < i + 10; j = j + 2)
+			for (j = i; j < i + 10; j++)
 			{
 				if (j < size)
-				{
-					printf("%02x%02x ", b[j], b[j + 1]);
-				}
+				{	printf("%02x", b[j]);	}
 				else
-				{
-					printf("     ");
-				}
+				{	printf("  ");	}
+				if (j % 2 != 0)
+				{	putchar(' ');	}
 			}
 			for (j = 0; j < 10; j++)
 			{
 				if (i < size)
 				{
 					if (b[i] >= 32 && b[i] <= 126)
-					{
-						putchar(b[i]);
-					}
+					{	putchar(b[i]);	}
 					else
-					{
-						putchar('.');
-					}
+					{	putchar('.');	}
 				}
 				i++;
 			}
