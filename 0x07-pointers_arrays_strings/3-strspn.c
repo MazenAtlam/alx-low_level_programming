b60260ebcb0f9888dbcf42c4900d2da33d236f79
MazenAtlam <mazenatef5510@gmail.com>
2023-07-31 14:01:03 -0700

    task3, 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,19 +11,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int bytes = 0;
+	unsigned int i, j;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (s[i] == accept[j])
+			if (accept[j] == '\0')
 			{
-				bytes++;
+				return (i);
 			}
 		}
 	}
 
-	return (bytes);
+	return (i);
 }
