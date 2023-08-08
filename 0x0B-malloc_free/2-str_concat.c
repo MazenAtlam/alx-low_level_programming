d8108219da30c7e41a54e4af4a4859fa7cf9a81b
MazenAtlam <mazenatef5510@gmail.com>
2023-08-08 16:35:17 -0700

    task2, 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = '\0';
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = '\0';
 	for (i = 0; s1[i] != '\0'; i++)
 		size1++;
@@ -32,9 +32,10 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2 != NULL)
 	{
-		for (i = size1 - 1, j = 0; j < size2; i++, j++)
+		for (i = size1, j = 0; j < size2; i++, j++)
 			s[i] = s2[j];
 	}
+	s[i] = '\0';
 
 	return (s);
 }
