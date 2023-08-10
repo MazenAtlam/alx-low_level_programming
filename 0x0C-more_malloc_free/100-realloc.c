8c95cfbfcf49a88ee1daaf9e265b2442881d87c1
MazenAtlam <mazenatef5510@gmail.com>
2023-08-10 14:27:03 -0700

    task4, 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,11 +16,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new;
 	unsigned int i;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 
-		return (NULL);	}
+		return (NULL);
+	}
 
 	if (new_size == old_size)
 		return (ptr);
