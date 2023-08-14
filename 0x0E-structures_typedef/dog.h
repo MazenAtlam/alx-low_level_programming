4134b72a9e4a77474699e964a7c472189e3cfc0d
MazenAtlam <mazenatef5510@gmail.com>
2023-08-14 16:57:04 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+typedef struct dog dog;
+
+/**
+ * struct dog - Poppy
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the person who owns this dog
+ *
+ * Description: Define a new type struct doig
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
