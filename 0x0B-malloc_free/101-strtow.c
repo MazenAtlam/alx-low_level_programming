f6129120cf88183fcc134889560269a163f0bfe6
MazenAtlam <mazenatef5510@gmail.com>
2023-08-15 15:54:40 -0700

    task6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * word_counter - A funcction that gets the number of words at a string
+ * @str: The string
+ *
+ * Return: The number of words
+*/
+
+int word_counter(char *str)
+{
+	int i, words = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+		{
+			words++;
+			while (str[i] != 32)
+				i++;
+		}
+	}
+
+	return (words);
+}
+
+/**
+ * strtow - A function that splits a string into words
+ * @str: The string
+ *
+ * Return: A pointer to to an array of strings which have the words
+*/
+
+char **strtow(char *str)
+{
+	int i, j, k, init, length = 0;
+	char **s;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	s = malloc((word_counter(str) + 1) * sizeof(*s));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0, k = 0; str[i] != '\0'; i++)
+	{
+		while (str[i] == 32)
+			i++;
+		init = i;
+		for (length = 0; str[i] != 32 && str[i] != '\0'; i++, length++)
+				;
+		if (length > 0)
+		{
+			s[k] = malloc((length + 1) * sizeof(char));
+			if (s[k] == NULL)
+			{
+				while (k--)
+					free(s[i]);
+				free(s);
+
+				return (NULL);
+			}
+			for (j = 0; j < length; j++, init++)
+				s[k][j] = str[init];
+			s[k][j] = '\0';
+			k++;
+		}
+	}
+	s[k] = NULL;
+
+	return (s);
+}
