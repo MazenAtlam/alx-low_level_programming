852276ffdd0bcd9a02c37d496dc3e6c20c98c79d
MazenAtlam <mazenatef5510@gmail.com>
2023-08-25 13:14:07 -0700

    task0, 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * _strlen A function that gets the length of a string
+ * @str: The string
+ *
+ * Return: The length of the string
+*/
+
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0' && str != NULL)
+		i++;
+
+	return (i);
+}
+
+/**
  * print_list - A function that prints all the elements of a list_t list
  * @h: The list to be printed
  *
@@ -19,7 +36,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("%s\n", h->str);
+			printf("[%d] %s\n", _strlen(h->str), h->str);
 		i++;
 	}
 
