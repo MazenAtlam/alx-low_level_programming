fe40c657fa8b89ee8172d63f45bd9ccc8720492a
MazenAtlam <mazenatef5510@gmail.com>
2023-08-31 11:29:11 -0700

    task2, 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,31 +1,6 @@
 #include "main.h"
 
 /**
- * binary_len - A function that gets the length of a number in binary
- * @n: The numner in decimal (base 10)
- *
- * Return: The length
- */
-unsigned int binary_len(unsigned long int n)
-{
-	unsigned int length = 0;
-
-	if (n == 0 || n == 1)
-		return (1);
-
-	if ((long int) n < 0)
-		return (64);
-
-	while (n != 0)
-	{
-		length++;
-		n = n >> 1;
-	}
-
-	return (length);
-}
-
-/**
  * get_bit - A function that returns the value of a bit at a given index
  * @n: The number in decimal (base 10)
  * @index: The index of the bit
@@ -34,16 +9,8 @@ unsigned int binary_len(unsigned long int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int len = binary_len(n);
-
-	if (index >= len || index >= 64)
+	if (index >= 64)
 		return (-1);
 
-	while (index != 0)
-	{
-		n = n >> 1;
-		index--;
-	}
-
-	return (n & (1L >> 0));
+	return ((n >> index) & 1);
 }
