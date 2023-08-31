ac8e4c71b20ca518024799b2a872d9a4753cd8a0
MazenAtlam <mazenatef5510@gmail.com>
2023-08-31 08:41:21 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * binary_len - A function that gets the length of a number in binary
+ * @n: The numner in decimal (base 10)
+ *
+ * Return: The length
+ */
+int binary_len(unsigned long int n)
+{
+	int length = 0;
+
+	if (n == 0 || n == 1)
+		return (1);
+
+	while (n != 0)
+	{
+		length++;
+		n = n >> 1;
+	}
+
+	return (length);
+}
+
+/**
+ * print_binary - A function that prints the binary representation of a number
+ * @n: The numner in decimal (base 10)
+ *
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int bit_pos = binary_len(n) - 1;
+
+	while (bit_pos >= 0)
+	{
+		if ((n & (1 << bit_pos)) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+		bit_pos--;
+	}
+}
