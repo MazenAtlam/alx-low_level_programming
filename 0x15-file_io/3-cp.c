436c03480a31986f197e8c51f5e2d31b1f9b6ff6
MazenAtlam <mazenatef5510@gmail.com>
2023-09-04 15:44:24 -0700

    task3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#define BUFF_SIZE 1024
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments entered
+ * @argv: Pointers to arrays that store this arguments
+ *
+ * Description: A program that copies the content of a file to another file
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	char *file_from = argv[1], *file_to = argv[2];
+	char buffer[BUFF_SIZE];
+	int fptr_from, fptr_to, no_close, bytes_from = 0, bytes_to = 0;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);	}
+	fptr_from = open(file_from, O_RDONLY);
+	if (fptr_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);	}
+	fptr_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fptr_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);	}
+	while (bytes_from != EOF)
+	{
+		bytes_from = read(fptr_from, buffer, BUFF_SIZE);
+		if (bytes_from == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);	}
+		bytes_to = write(fptr_to, buffer, BUFF_SIZE);
+		if (bytes_to != bytes_from)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);	}	}
+	fptr_from = close(fptr_from);
+	fptr_to = close(fptr_to);
+	if (fptr_from != 0 || fptr_to != 0)
+	{
+		no_close = (fptr_from != 0) ? fptr_from : fptr_to;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", no_close);
+		exit(100);	}
+	return (0);
+}
