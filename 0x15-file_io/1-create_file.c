f78ca267c03f79f5c2e4a2a2cfec16440d37f096
MazenAtlam <mazenatef5510@gmail.com>
2023-09-04 13:39:07 -0700

    task1, 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,23 +27,22 @@ ssize_t _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t bytes, length;
+	ssize_t bytes, length = _strlen(text_content);
 	int fptr;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-	length = _strlen(text_content);
 	fptr = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fptr == -1)
 		return (-1);
 
-	bytes = write(fptr, text_content, length);
-	if (bytes == -1 || bytes != length)
-		return (-1);
-
+	if (length != 0)
+	{
+		bytes = write(fptr, text_content, length);
+		if (bytes == -1 || bytes != length)
+			return (-1);
+	}
 	close(fptr);
 
 	return (1);
