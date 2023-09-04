30135c604b905abcbcb957b3083d826fbe34ff2a
MazenAtlam <mazenatef5510@gmail.com>
2023-09-04 13:42:07 -0700

    task1, 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fptr = open(filename, O_CREAT | O_WRONLY, 0600);
+	fptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fptr == -1)
 		return (-1);
 
