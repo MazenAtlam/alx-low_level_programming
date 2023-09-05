cb50a377b4f12cb63e685cc4783e0b73ef7aacf0
MazenAtlam <mazenatef5510@gmail.com>
2023-09-05 14:05:49 -0700

    task4, error condition

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @ac: The number of arguments
+ * @av: pointers to this arguments
+ *
+ * Description: Empty
+ *
+ * Return: 98 (on error)
+ */
+int main(int ac, char *av[])
+{
+	return (98);
+}
