05e45565591b0f97e3e72ee48de34ac76f03db14
MazenAtlam <mazenatef5510@gmail.com>
2024-02-05 21:04:45 +0000

    task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,15 +10,24 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current_item;
-	unsigned long int i, j;
+	unsigned long int i;
+	bool first_print = true;
+
+	if (!ht || !ht->array)
+		return;
 
 	printf("{");
-	if (ht != NULL && ht->size != 0 && ht->array != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		current_item = ht->array[i];
+		while (current_item)
 		{
-			if
+			if (!first_print)
+				printf(", ");
+			printf("\'%s\': \'%s\'", current_item->key, current_item->value);
+			first_print = false;
+			current_item = current_item->next;
 		}
 	}
-	printf("}");
+	printf("}\n");
 }
